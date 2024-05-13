7290d3cea8eac6d47cc9601b957c923f61a71c79
ArauSAAA <167267030+ArauSAAA@users.noreply.github.com>
2024-05-13 17:41:35 -0400

    Update tareas.py

--- tareas.py
+++ tareas.py
@@ -11,23 +11,51 @@ class Tareas:
         self.entrada= Entry(self.ventana)
         self.entrada.grid(row=0,column=0)
 
-        self.botonAgregar= Button(self.ventana, text='agregar')
+        self.botonAgregar= Button(self.ventana, text='agregar', command=self.agregar)
         self.botonAgregar.grid(row=0,column=1)
 
-        self.botonQuitar= Button(self.ventana, text='quitar')
+        self.botonQuitar= Button(self.ventana, text='quitar', command=self.quitar)
         self.botonQuitar.grid(row=0,column=2)
+
+        self.tareasNoHecha= StringVar(value=self.tareasNoHechas)
+        self.tareasHecha= StringVar(value=self.tareasHechas)
+
+        self.lista=Listbox(self.ventana, listvariable=self.tareasNoHecha)
+        self.lista.grid(row=1,column=0)
+
+
+        self.lista2=Listbox(self.ventana, listvariable=self.tareasHecha)
+        self.lista2.grid(row=1,column=1)
+
+
 
     def correr(self):
         self.ventana.mainloop()
 
     def agregar(self):
-        pass
+        self.texto= self.entrada.get()
+        if  self.texto == '':
+            pass
+        else:
+            self.tareasNoHechas.append(self.texto)
+            self.lista.insert(len(self.tareasNoHechas), f'{self.texto}❌')
+            self.entrada.delete(0, END)
 
-    def quitar(selfg):
-        pass
+
+    def quitar(self):
+        
+        self.cosa= self.lista.curselection()
+
+        self.tareasHechas.insert(self.cosa[0], self.lista2.get(self.cosa[0]) )
+
+        self.lista2.insert(len(self.tareasHechas), f'{self.lista2.get(self.cosa[0])}✅')
+
+        self.tareasNoHechas.pop(self.cosa[0])
+        self.lista.delete(self.cosa)
+
+        
+        
 
 
-
-x=Tareas()
-
-x.correr()
+app=Tareas()
+app.correr()
