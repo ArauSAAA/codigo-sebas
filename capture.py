d81c7619152f1aa48a9a045697e09e55b424e7a2
ArauSAAA <167267030+ArauSAAA@users.noreply.github.com>
2024-05-16 17:56:32 -0400

    Add files via upload

---
+++ capture.py
@@ -0,0 +1,24 @@
+import pyautogui
+
+import time
+from tkinter import *
+
+def tomarFoto():
+    texto= entrada.get()
+    time.sleep(5)
+    foto= pyautogui.screenshot()
+    foto.save(f'{texto}.png')
+    entrada.delete(0,END)
+
+ventana= Tk()
+
+entrada= Entry(ventana)
+entrada.grid(row=0,column=0)
+
+boton= Button(ventana, text='tomar foto', command=tomarFoto)
+boton.grid(row=1, column=0, padx=10, pady=10)
+
+
+ventana.mainloop()
+
+
